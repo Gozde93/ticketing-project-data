673896fa6dece91c91e5ad54b0495b7dc5feec85
gozdediri <dirigozde@gmail.com>
2024-05-16 00:15:15 -0500

    ticketing-data part2

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
     User findByUserName(String username);
+    @Transactional
+    void deleteByUserName(String username);
 }
--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -52,6 +52,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteByUserName(String username) {
+        userRepository.deleteByUserName(username);
 
     }
 
